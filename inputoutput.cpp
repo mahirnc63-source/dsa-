b2efc92642a3662c6a8026605c73e09598001a6d
Mahi_Singh061 <mahirnc63@gmail.com>
2026-05-25 02:24:04 +0530

    added file

---
+++ inputoutput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void printNumber() {
+      int a;
+      cout<<"enter number to be printed";
+      cin>>a;
+      cout<<a;
+    }
+int main(){
+    printNumber();
+    return 0;
+}
