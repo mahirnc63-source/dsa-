4436f5871e50941901e3ed3ad408793fc7beee08
Mahi_Singh061 <mahirnc63@gmail.com>
2026-05-27 03:28:32 +0530

    pattern2 updated

--- patterns.cpp
+++ patterns.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 using namespace std;
-void pattern1(){
+/*void pattern1(){
     for(int i=1;i<=5;i++){
         for(int j=1;j<=i;j++){
             cout<<"*";
         }
         cout<<endl;
     }
+}*/
+void pattern2(){
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+
+        }
+        cout<<endl;
+    }
 }
+
 int main(){
-    pattern1();
+    //pattern1();
+    pattern2();
     return 0;
 
 }
