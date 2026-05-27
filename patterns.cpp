3c49f55c2b300e3eec3d196b0208546bbea14ddb
Mahi_Singh061 <mahirnc63@gmail.com>
2026-05-27 23:28:22 +0530

    pattern6

--- patterns.cpp
+++ patterns.cpp
@@ -47,10 +47,35 @@ void pattern4(int n){
     
 }
 
+void pattern5(int n){
+    for(int i=1;i<=n;i++){
+        //space
+        for(int j=1;j<=i-1;j++){
+            cout<<" ";
+            
+
+        }
+        //star
+        for(int j=1;j<=2*n-(2*i-1);j++){
+            cout<<"*";
+        }
+        //space
+        for(int j=1;j<=i-1;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
+void pattern6(){
+    pattern4(5);
+    pattern5(5);
+}
+
 int main(){
     //pattern1();
     //pattern2();
-    pattern4(5);
+    pattern6();
     return 0;
 
 }
