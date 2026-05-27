ca7aef17778251ac443e6f9f199045f653953a40
Mahi_Singh061 <mahirnc63@gmail.com>
2026-05-28 02:33:58 +0530

    .

--- patterns.cpp
+++ patterns.cpp
@@ -167,11 +167,24 @@ void pattern12(int n){
     }
     
 }
+void pattern13(int n){
+    
+    for(int i=1;i<=n;i++){
+        char ch='A';
+        ch=ch+(n-1)-i+1;
+        for(int j=1;j<=i;j++){
+            
+            cout<<ch;
+            ch++;
+        }
+        cout<<endl;
+    }
+}
 
 int main(){
     //pattern1();
     //pattern2();
-    pattern12(5);
+    pattern13(5);
     return 0;
 
 }
