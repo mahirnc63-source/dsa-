37e03406e71e88b6414556bb757acdbb8fa317b5
Mahi_Singh061 <mahirnc63@gmail.com>
2026-05-27 23:10:32 +0530

    pattern4

--- patterns.cpp
+++ patterns.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-/*void pattern1(){
+void pattern1(){
     for(int i=1;i<=5;i++){
         for(int j=1;j<=i;j++){
             cout<<"*";
@@ -16,7 +16,7 @@ void pattern2(){
         }
         cout<<endl;
     }
-}*/
+}
 void pattern3(){
     for(int i=1;i<=5;i++){
         for(int j=5;j>=i;j--){
@@ -26,11 +26,31 @@ void pattern3(){
         cout<<endl;
     }
 }
+void pattern4(int n){
+    for(int i=1;i<=n;i++){
+        //space
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+            
+
+        }
+        //star
+        for(int j=1;j<=2*i-1;j++){
+            cout<<"*";
+        }
+        //space
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
 
 int main(){
     //pattern1();
     //pattern2();
-    pattern3();
+    pattern4(5);
     return 0;
 
 }
