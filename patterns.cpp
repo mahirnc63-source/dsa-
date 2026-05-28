8793b1a8f16a0d3f4832665df135028a629a7600
Mahi_Singh061 <mahirnc63@gmail.com>
2026-05-29 01:01:30 +0530

    updated

--- patterns.cpp
+++ patterns.cpp
@@ -207,10 +207,25 @@ pattern14(int n){
         cout<<endl;
     }
 }
+void pattern15(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==1||i==n||j==1||j==n){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+            
+        }
+        cout<<endl;
+    }
+
+}
 int main(){
     //pattern1();
     //pattern2();
-    pattern14(5);
+    pattern15(6);
     return 0;
 
 }
