d455e240e54f55c5eb0e2ed211d23a5566a247e8
Mahi_Singh061 <mahirnc63@gmail.com>
2026-05-30 23:55:25 +0530

    .

--- basicmaths.cpp
+++ basicmaths.cpp
@@ -9,7 +9,18 @@ void countdigit(int n){
     }
     cout<<count;
 }
+void reverse(int n){
+    int rem=0;
+    int rev=0;
+    while(n>0){
+        rem=n%10;
+        n=n/10;
+        rev=rev*10+rem;
+    }
+    cout<<rev;
+}
 int main(){
-    countdigit(467890654);
+    //countdigit(467890654);
+    reverse(4567);
     return 0;
 }
