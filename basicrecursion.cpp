7f107966a3f4cf3c93aa82539fac61c8f01cc3df
Mahi_Singh061 <mahirnc63@gmail.com>
2026-06-04 00:33:47 +0530

    .

--- basicrecursion.cpp
+++ basicrecursion.cpp
@@ -20,11 +20,15 @@ int sum(int n){
     if(n==0) return 0;
     return n+sum(n-1);
 }
+int factorial(int n){
+    if(n==0) return 1;
+    return n*factorial(n-1);
+}
 int main(){
     //f(1,4);
     //f1(4,4);
     //f2(4,4);
-    cout<<sum(3);
+    cout<<factorial(5);
     return 0;
     
 }
