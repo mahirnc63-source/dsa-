043c30ac1053a7196be688c0724a026c8cf51aba
Mahi_Singh061 <mahirnc63@gmail.com>
2026-06-03 22:39:04 +0530

    updated

--- basicrecursion.cpp
+++ basicrecursion.cpp
@@ -11,8 +11,14 @@ void f1(int i,int n){
     f1(i-1,n);
     cout<<i<<endl;
 }
+void f2(int i,int n){
+    if(i<1) return;
+    cout<<i<<endl;
+    f2(i-1,n);
+}
 int main(){
-    f(1,4);
-    f1(4,4);
+    //f(1,4);
+    //f1(4,4);
+    f2(4,4);
     
 }
