793dce52a05317b11b882abb3240c4b5759fd68f
Mahi_Singh061 <mahirnc63@gmail.com>
2026-06-05 21:58:11 +0530

    fibonacci

--- basicrecursion.cpp
+++ basicrecursion.cpp
@@ -29,17 +29,25 @@ void rarray(int i,int arr[],int n){
     swap(arr[i],arr[n-i-1]);
     rarray(i+1,arr,n);
 }
+int fibo(int n){
+    if(n<=1) return n;
+    int last=fibo(n-1);
+    int slast=fibo(n-2);
+    return last+slast;
+
+}
 int main(){
     //f(1,4);
     //f1(4,4);
     //f2(4,4);
     //cout<<factorial(5);
-    int n;
+    /*int n;
     cin>>n;
     int arr[n];
     for(int i=0;i<n;i++) cin>>arr[i];
     rarray(0,arr,n);
     for(int i=0;i<n;i++) cout<<arr[i]<<" ";
-    return 0;
+    return 0;*/
+    cout<<fibo(6);
     
 }
