644531c66e6ef47824cbc768487d5a1c6f6a1395
Mahi_Singh061 <mahirnc63@gmail.com>
2026-06-07 21:27:41 +0530

    map

--- hashing.cpp
+++ hashing.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 int main(){
     int n;
@@ -12,14 +12,47 @@ int main(){
         mpp[a[i]]++;
     }
 
-    /*int hash[13]={0};
+    int hash[13]={0};
     for(int i=0;i<n;i++){
         hash[a[i]]+=1;
-    }*/
+    }
     for (auto x : mpp) {
         cout << x.first << " " << x.second << endl;
     }
 
+
+    return 0;
+}*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    map<int, int> freq;
+
+    for(int x : nums) {
+        freq[x]++;
+    }
+
+    int maxFreq = 0;
+    int ans;
+
+    for(auto x : freq) {
+        if(x.second > maxFreq) {
+            maxFreq = x.second;
+            ans = x.first;
+        }
+    }
+
+    cout << ans;
 
     return 0;
 }
