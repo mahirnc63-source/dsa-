fa878ae3c795a08beb2f8ab65fb2e19a50f766b2
Mahi_Singh061 <mahirnc63@gmail.com>
2026-06-17 16:58:47 +0530

    new

--- sorting.cpp
+++ sorting.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 void selectionsort(int arr[],int n){
+
     for(int i=0;i<n-1;i++){
         int mini=i;
         for(int j=i+1;j<n;j++){
@@ -19,9 +20,25 @@ void selectionsort(int arr[],int n){
 
 
 }
+void bubblesort(int arr[],int n){
+    for(int i=n-1;i>=0;i--){
+        for(int j=0;j<=i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+            }
+
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
 int main(){
     int arr[]={13,26,48,4,9};
     int n = sizeof(arr) / sizeof(arr[0]);
-    selectionsort(arr,n);
+    //selectionsort(arr,n);
+    bubblesort(arr,n);
     return 0;
 }
