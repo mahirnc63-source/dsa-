e1b6849e3d2a433aa61191d0ccafe8a15c34fc3b
Mahi_Singh061 <mahirnc63@gmail.com>
2026-06-23 23:14:22 +0530

    .

--- sorting.cpp
+++ sorting.cpp
@@ -100,14 +100,39 @@ void mergesort(int arr[],int n){
     
 
 }
+int partition(int arr[],int low,int high){
+    int pivot=arr[high];
+    int i=low-1;
+    for(int j=low;j<high;j++){
+        if(arr[j]<=pivot){
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[i+1],arr[high]);
+    return i+1;
 
+}
+void quicksort(int arr[],int low ,int high){
+    if(low<high){
+       int pindex=partition(arr,low,high);
+       quicksort(arr,low,pindex-1);
+       quicksort(arr,pindex+1,high);
+
+    }
+
+}
 int main(){
     int arr[]={13,26,48,4,9};
     int n = sizeof(arr) / sizeof(arr[0]);
     //selectionsort(arr,n);
     //bubblesort(arr,n);
     //insertionsort(arr,n);
-    mergesort(arr,n);
+    //mergesort(arr,n);
+    quicksort(arr,0,n-1);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
 
     return 0;
 }
