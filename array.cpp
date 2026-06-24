b3ce0865fd0b617a5b179b2bf25f85b2f7cd595f
Mahi_Singh061 <mahirnc63@gmail.com>
2026-06-24 23:13:52 +0530

    .

--- array.cpp
+++ array.cpp
@@ -9,11 +9,26 @@ int largest(int arr[],int n){
     }
     return large;
 }
+int secondlargest(int arr[],int n){
+    int large=arr[0];
+    int slarge=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i]>large){
+            slarge=large;
+            large=arr[i];
+        }
+        else if(arr[i]<large&&arr[i]>slarge){
+            slarge=arr[i];
+        }
+    }
+    return slarge;
+
+}
 int main(){
     int arr[6]={2,4,8,6,9};;
     int n=sizeof(arr) / sizeof(arr[0]);
-    int a=largest(arr,n);
-    cout<<"largest="<<a;
+    int a=secondlargest(arr,n);
+    cout<<"slargest="<<a;
 
     return 0;
 }
